15d497bb61cb81365055ec932657524474c5b129
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-23 21:11:33 -0300

    Add files via upload

--- Lab6Aula1/Laboratorio6/Banco.cs
+++ Lab6Aula1/Laboratorio6/Banco.cs
@@ -6,44 +6,8 @@ using System.Threading.Tasks;
 
 namespace Laboratorio6
 {
-    class Banco
+    public class Banco
     {
-        private List<Conta> contas;
-        private object pessoa;
-        private double valor;
-
-        public Banco() 
-        {
-            contas = new List<Conta>();
-        }
-
-        public void AddConta(Pessoa p) 
-        {
-            contas.Add(p.Money);
-        }
-        public void Depositar(Pessoa p, double v) 
-        {
-            for (int i = 0; i < contas.Count; i++)
-            {
-                if (contas[i].ID==p.Money.ID)
-                {
-                    contas[i].Valor += valor;
-                }
-                
-            }
-        }
-        public double Saldo(Pessoa p) 
-        {
-            double saldo = 0.0;
-            for (int i = 0; i < contas.Count; i++)
-            {
-                if (contas[i].ID == p.Money.ID) 
-                {
-                    saldo = contas[i].Valor;
-                }
-            }
-            return saldo;
-        }
-
+        public string modelo { get; set}
     }
 }
--- Lab6Aula1/Laboratorio6/Conta.cs
+++ Lab6Aula1/Laboratorio6/Conta.cs
@@ -7,51 +7,56 @@ using System.Threading.Tasks;
 namespace Laboratorio6
 {
     public abstract class Conta
-                 
-    {
-        public int ID { get; set; }
-        public double Valor { get; set; }
 
-        private decimal saldo;
+    {
+
+        private decimal saldo; 
         private string titular;
+
+
+        //public int numero { get; set; }
+        //public double Valor { get; set; }
+
+        //public decimal saldo { get; set; }
+        //private string titular;
 
         public Conta(string t)
         {
             titular = t;
         }
 
-        public decimal Saldo
-        {
-            get { return saldo; }
-        }
+            public decimal Saldo
+            {
+                get { return saldo; }
+            }
 
-        public string Titular
-        {
-            get { return titular; }
-        }
-        public abstract string Id
-        {
-            get;
-        }
-        public virtual void Depositar(decimal valor)
-        {
-            saldo += valor;
-        }
+            public string Titular
+            {
+                get { return titular; }
+            }
+            public abstract string Id
+            {
+                get;
+            }
+            public virtual void Depositar(decimal valor)
+            {
+                saldo += valor;
+            }
 
-        public virtual void Sacar(decimal valor)
-        {
-            saldo -= valor;
-        }
+            public virtual void Sacar(decimal valor)
+            {
+                saldo -= valor;
+            }
 
-        public override bool Equals(object obj)
-        {
-            return obj is Conta conta &&
-                   Titular == conta.Titular;
-        }
+            public override bool Equals(object obj)
+            {
+                return obj is Conta conta &&
+                       Titular == conta.Titular;
+            }
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Titular);
-        }
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(Titular);
+            }
     }
 }
